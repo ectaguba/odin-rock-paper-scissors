58c96a3f1696c4cc8ecec3dbfaebd341623520a6
niichirin <itschristaguba@gmail.com>
2022-10-23 19:05:02 -0700

    Fixed everything in rps.js

--- rps.js
+++ rps.js
@@ -1,3 +1,24 @@
+// Initalize results counter
+const results = document.querySelector("#round-results");
+const playerScore = document.querySelector('#player-score');
+const computerScore = document.querySelector('#computer-score');
+
+let playerPoints = 0;
+let computerPoints = 0;
+
+// Iterate through each button
+const optionBtns = document.querySelectorAll('.btn');
+optionBtns.forEach((button) => {
+    let playerSelection = button.textContent;
+    button.addEventListener("click", () => playRound(playerSelection, getComputerChoice()));
+})
+
+// Add event listeners to each button
+
+// Pass button textContent to playerSelection parameter
+
+
+// Randomize computer choice
 function getComputerChoice() {
     move = Math.floor(Math.random() * (4 - 1) + 1);
     switch (move) {
@@ -7,37 +28,61 @@ function getComputerChoice() {
     }
 }
 
+// Click -> Play a round
 function playRound(playerSelection = "No input was received.", computerSelection) {
-    console.log("onclick is working");
-    let results = document.querySelector("#results");
 
-    if (playerSelection = "rock") {
+    // Compare player selections to computer selection
+    // Increment counter
+    // Animate counter and results (separate function)
+    if (playerSelection == "Rock") {
         switch (computerSelection) {
-            case "rock": results.textContent = "You tied!"; break;
-            case "paper": results.textContent = "You lose. Paper beats Rock."; break;
-            case "scissors": results.textContent = "You win! Rock beats Scissors."; break;
+            case "rock": 
+                results.textContent = "You tied!"; 
+                break;
+            case "paper": 
+                results.textContent = "You lose. Paper beats Rock."; 
+                computerScore.textContent = ++computerPoints;
+                break;
+            case "scissors": 
+                results.textContent = "You win! Rock beats Scissors."; 
+                playerScore.textContent = ++playerPoints;
+                break;
         }
-    } else if (playerSelection = "paper") {
+    } else if (playerSelection == "Paper") {
         switch (computerSelection) {
-            case "rock": results.textContent = "You win! Paper beats Rock."; break;
-            case "paper": results.textContent = "You tied!"; break;
-            case "scissors": results.textContent = "You tied!"; break;
+            case "rock": 
+                results.textContent = "You win! Paper beats Rock.";
+                playerScore.textContent = ++playerPoints;
+                break;
+            case "paper": 
+                results.textContent = "You tied!"; 
+                break;
+            case "scissors": 
+                results.textContent = "You lose. Scissors beats Rock."; 
+                computerScore.textContent = ++computerPoints;
+                break;
         }
 
-    } else if (playerSelection = "scissors") {
+    } else if (playerSelection == "scissors") {
         switch (computerSelection) {
-            case "rock": results.textContent = "You lose. Rock beats Scissors."; break;
-            case "paper": results.textContent = "You win! Scissors beats Paper."; break;
-            case "scissors": results.textContent = "You tied!"; break;
+            case "rock": 
+                results.textContent = "You lose. Rock beats Scissors.";
+                computerScore.textContent = ++computerPoints;
+                break;
+            case "paper": 
+                results.textContent = "You win! Scissors beats Paper."; 
+                playerScore.textContent = ++playerPoints;
+                break;
+            case "scissors": 
+                results.textContent = "You tied!"; 
+                break;
         }
     }
 }
 
-const rock = document.querySelector("#rockBtn");
-rock.addEventListener("click", playRound("rock", getComputerChoice()));
+function animateResults(winner) {
+    if (winner == "player") {
+        
+    }
 
-const paper = document.querySelector("#paperBtn");
-paper.addEventListener("click", playRound("paper", getComputerChoice()));
-
-const scissors = document.querySelector("#scissorsBtn")
-scissors.addEventListener("click", playRound("scissors", getComputerChoice()));
+}
